7c4f58fa892b2994b0b4f6bad2f0a9d4dcaf277b
jovanaikovic <joxjovana@gmail.com>
2023-09-24 13:31:54 +0200

    Vehicle model defined & migrations made

--- car_app/models.py
+++ car_app/models.py
@@ -5,3 +5,28 @@ from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
     pass
+
+class Vehicle(models.Model):
+
+    FUEL_CHOICES = [
+        ('Benzin', 'Benzin'),
+        ('Dizel', 'Dizel'),
+        ('Električno', 'Električno'),
+        # Add more choices as needed
+    ]
+
+    TRANSMISSION_CHOICES = [
+        ('Automatik', 'Automatik'),
+        ('Manual', 'Manual'),
+        ('DSG' , 'DSG'),
+        # Add more choices as needed
+    ]
+
+
+    vehicle_make = models.CharField(max_length=100)
+    vehicle_model = models.CharField(max_length=100)
+    year_of_manufacturing = models.PositiveIntegerField()
+    fuel_type = models.CharField(max_length=100, choices=FUEL_CHOICES)
+    transmission = models.CharField(max_length=100, choices=TRANSMISSION_CHOICES)
+    door_count = models.PositiveIntegerField()
+    vehicle_price = models.DecimalField(max_digits=10, decimal_places=2)
