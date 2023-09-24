a69237633268a59217242fd8d011c1fbaa5c3e73
jovanaikovic <joxjovana@gmail.com>
2023-09-24 20:46:57 +0200

    Simple view to get JSON data

--- car_app/views.py
+++ car_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from .models import Vehicle
+
+class VehicleListView(View):
+    def get(self, request, *args, **kwargs):
+        # Retrieve the list of vehicles
+        vehicles = Vehicle.objects.all().values()
+
+        # Convert the queryset to a list and return as JSON response
+        return JsonResponse(list(vehicles), safe=False)
