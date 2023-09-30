1bc616d7e8809735ee0efc3f40d7ea2ec958d312
jovanaikovic <joxjovana@gmail.com>
2023-09-30 15:02:35 +0200

    Token lifetime extended

--- car_sales/settings.py
+++ car_sales/settings.py
@@ -136,3 +136,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+
+}
